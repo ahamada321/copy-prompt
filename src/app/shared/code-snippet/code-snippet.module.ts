6172ef9b079fe62cf01049a4912c2e4c26e34f3f
ahamada321 <a.hamada.biz@gmail.com>
2024-01-29 17:14:18 +0900

    In the middle of development

--- src/app/shared/code-snippet/code-snippet.module.ts
+++ src/app/shared/code-snippet/code-snippet.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { CodeSnippetComponent } from './code-snippet.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { CodeSnippetComponent } from "./code-snippet.component";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   declarations: [CodeSnippetComponent],
   exports: [CodeSnippetComponent],
 })

24b9dae25c6c2f96c401298cd8eb45c812617ab6
ahamada321 <a.hamada.biz@gmail.com>
2024-01-29 14:34:06 +0900

    In the middle of development

--- src/app/prompt/prompt-list/prompt-list-item/prompt-list-item.component.ts
+++ src/app/prompt/prompt-list/prompt-list-item/prompt-list-item.component.ts
@@ -1,49 +1,25 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { MyOriginAuthService } from 'src/app/auth/shared/auth.service';
-import { PromptService } from '../../shared/prompt.service';
-import { Prompt } from '../../shared/prompt.model';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { LoginPopupComponent } from 'src/app/auth/login-popup/login-popup.component';
+import { Component, OnInit, Input } from "@angular/core";
+import { MyOriginAuthService } from "src/app/auth/shared/auth.service";
+import { Prompt } from "../../shared/prompt.model";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { LoginPopupComponent } from "src/app/auth/login-popup/login-popup.component";
 
 @Component({
-  selector: 'app-prompt-list-item',
-  templateUrl: './prompt-list-item.component.html',
-  styleUrls: ['./prompt-list-item.component.scss'],
+  selector: "app-prompt-list-item",
+  templateUrl: "./prompt-list-item.component.html",
+  styleUrls: ["./prompt-list-item.component.scss"],
 })
 export class PromptListItemComponent implements OnInit {
   @Input() prompt!: Prompt;
-  isFavorite!: boolean;
 
   constructor(
     public auth: MyOriginAuthService,
-    private modalService: NgbModal,
-    private promptService: PromptService
+    private modalService: NgbModal
   ) {}
 
-  ngOnInit() {
-    // this.initFavourite();
-  }
-
-  // private initFavourite() {
-  //   const index = this.prompt.favoritesFrom.indexOf(this.auth.getUserId());
-  //   this.isFavorite = index >= 0;
-  // }
-
-  // toggleFavourite() {
-  //   this.promptService.toggleFavourite(this.prompt._id).subscribe(
-  //     (index) => {
-  //       if (index >= 0) {
-  //         this.prompt.favoritesFrom.splice(index, 1); // Dlete user from array.
-  //       } else {
-  //         this.prompt.favoritesFrom.push(this.auth.getUserId());
-  //       }
-  //       this.isFavorite = !(index >= 0); // Be careful. Need to return opposite.
-  //     },
-  //     (error) => {}
-  //   );
-  // }
+  ngOnInit() {}
 
   modalLoginOpen() {
-    this.modalService.open(LoginPopupComponent, { backdrop: 'static' });
+    this.modalService.open(LoginPopupComponent, { backdrop: "static" });
   }
 }
--- src/app/prompt/prompt-list/prompt-list-latest/prompt-list-latest.component.ts
+++ src/app/prompt/prompt-list/prompt-list-latest/prompt-list-latest.component.ts
@@ -1,25 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { Prompt } from '../../shared/prompt.model';
-import { PromptService } from '../../shared/prompt.service';
-import { MyOriginAuthService } from 'src/app/auth/shared/auth.service';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { Prompt } from "../../shared/prompt.model";
+import { PromptService } from "../../shared/prompt.service";
+import { MyOriginAuthService } from "src/app/auth/shared/auth.service";
 
 @Component({
-  selector: 'app-prompt-list-latest',
-  templateUrl: './prompt-list-latest.component.html',
-  styleUrls: ['./prompt-list-latest.component.scss'],
+  selector: "app-prompt-list-latest",
+  templateUrl: "./prompt-list-latest.component.html",
+  styleUrls: ["./prompt-list-latest.component.scss"],
 })
 export class PromptListLatestComponent implements OnInit {
-  prompts: Prompt[] = [];
+  prompts!: Prompt[];
   pageIndex: number = 1;
   pageSize: number = 5; // Displaying contents per page.
   pageCollectionSize: number = 1;
 
   constructor(
     private promptService: PromptService,
-    public auth: MyOriginAuthService,
-    private router: Router,
-    private route: ActivatedRoute
+    public auth: MyOriginAuthService
   ) {}
 
   ngOnInit() {
@@ -27,21 +24,19 @@ export class PromptListLatestComponent implements OnInit {
   }
 
   getLatestPrompts() {
-    this.route.queryParams.subscribe((keywords) => {
-      this.promptService
-        .getLatestPrompts(this.pageIndex, this.pageSize)
-        .subscribe(
-          (result) => {
-            this.prompts = result[0].foundPrompts;
-            if (this.prompts.length > 0) {
-              this.pageCollectionSize = result[0].metadata[0].total;
-            }
-          },
-          (err) => {
-            console.error(err);
+    this.promptService
+      .getLatestPrompts(this.pageIndex, this.pageSize)
+      .subscribe(
+        (result) => {
+          this.prompts = result[0].foundPrompts;
+          if (this.prompts.length > 0) {
+            this.pageCollectionSize = result[0].metadata[0].total;
           }
-        );
-    });
+        },
+        (err) => {
+          console.error(err);
+        }
+      );
   }
 
   pageChange() {

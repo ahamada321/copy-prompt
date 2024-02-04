092ffb7c578560221e053d58c64a9515db54a726
ahamada321 <a.hamada.biz@gmail.com>
2024-02-04 17:45:35 +0900

    In the middle of development

---
+++ src/app/prompt/prompt-list/prompt-list-category/prompt-list-category.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prompt-list-category',
+  templateUrl: './prompt-list-category.component.html',
+  styleUrls: ['./prompt-list-category.component.scss'],
+})
+export class PromptListCategoryComponent implements OnInit {
+  constructor() {}
+  ngOnInit() {}
+}

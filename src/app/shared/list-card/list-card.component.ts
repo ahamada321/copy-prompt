114ec706948146326c52758ae6ee686e3ee1ec2d
ahamada321 <a.hamada.biz@gmail.com>
2024-02-05 19:31:14 +0900

    Clean code

--- src/app/shared/list-card/list-card.component.ts
+++ src/app/shared/list-card/list-card.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { MyOriginAuthService } from 'src/app/auth/shared/auth.service';
 import { Prompt } from '../../prompt/shared/prompt.model';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { LoginPopupComponent } from 'src/app/auth/login-popup/login-popup.component';
 
 @Component({
   selector: 'app-list-card',
@@ -12,14 +9,7 @@ import { LoginPopupComponent } from 'src/app/auth/login-popup/login-popup.compon
 export class ListCardComponent implements OnInit {
   @Input() prompt!: Prompt;
 
-  constructor(
-    public auth: MyOriginAuthService,
-    private modalService: NgbModal
-  ) {}
+  constructor() {}
 
   ngOnInit() {}
-
-  modalLoginOpen() {
-    this.modalService.open(LoginPopupComponent, { backdrop: 'static' });
-  }
 }

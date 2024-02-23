d70ef7a574479251e97a8b6657734ac8b5d10250
Mai <manananahashi@gmail.com>
2024-02-23 11:45:02 +0900

    guide-button-2 done

--- src/app/shared/guide-button/guide-button.module.ts
+++ src/app/shared/guide-button/guide-button.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { GuideButtonComponent } from './guide-button.component';
 import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule, NgbModule],
   declarations: [GuideButtonComponent],
   exports: [GuideButtonComponent],
 })

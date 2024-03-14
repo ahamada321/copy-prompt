163f30803fc80d51273e5ec56ccbb852bfe222dd
Mai <manananahashi@gmail.com>
2024-03-14 12:43:34 +0900

    In the middle of development

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -9,6 +9,7 @@ import { BlogDetail2Component } from './blog-detail2/blog-detail2.component';
 import { BlogDetail3Component } from './blog-detail3/blog-detail3.component';
 import { BlogDetail4Component } from './blog-detail4/blog-detail4.component';
 import { BlogDetail5Component } from './blog-detail5/blog-detail5.component';
+import { BlogDetail6Component } from './blog-detail6/blog-detail6.component';
 
 const routes: Routes = [
   {
@@ -20,6 +21,7 @@ const routes: Routes = [
       { path: 'mail-communication', component: BlogDetail3Component },
       { path: 'how-to-use-prompt', component: BlogDetail4Component },
       { path: 'how-to-use-chatgpt', component: BlogDetail5Component },
+      { path: 'tutorial', component: BlogDetail6Component },
     ],
   },
 ];
@@ -32,6 +34,7 @@ const routes: Routes = [
     BlogDetail3Component,
     BlogDetail4Component,
     BlogDetail5Component,
+    BlogDetail6Component,
     BlogChatComponent,
   ],
   imports: [CommonModule, RouterModule.forChild(routes), ListCardModule],
